e49d71561310fa5b57261bf9a467dc6d12821ba7
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2023-03-26 04:45:08 +0200

    mongo-memory

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import connect from "./database/conn.js";
 
-const app= express()
+const app = express()
 
 //middleware
 app.use(express.json())
@@ -10,14 +11,22 @@ app.use(cors())
 app.use(morgan('tiny'))
 app.disable('x-powered-by')
 
-const port=8080;
+const port = 8080;
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.status(201).json("Home get request")
 })
 
-// start server
-app.listen(port,()=>{
-    console.log(`Server Connected to http://localhost:${port}`)
+// start server only when we have valid connection
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server Connected to http://localhost:${port}`)
+        })
+    } catch (error) {
+        console.log("Server cannot connect")
+    }
+}).catch(error=>{
+    console.log("Invalid Connection")
 })
 
